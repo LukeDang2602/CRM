ce61784ffc1cbef71532b50d6ff9a61e575d8f1a
Jake Dang <jakedang2602@gmail.com>
2022-11-10 18:29:29 -0600

    added handleCreateTask function to store task object in firestore

--- src/pages/CreateTask.js
+++ src/pages/CreateTask.js
@@ -1,41 +1,72 @@
-import React, { useState } from 'react'
-import { collection, addDoc } from 'firebase/firestore'
-import { db } from '../firebase-config'
+import React, { useState, useRef } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase-config';
 
 export default function CreateTask() {
-    //create a task object 
-    let taskObj ={
-        text: '',
-        date: '',
-        status: '',
-        customer:''
-    };
+    //declare states
+    const [task,setTask] = useState("");
+    const taskRef = useRef();
+    const [date,setDate] = useState("");
+    const dateRef = useRef();
+    const [priority,setPriority] = useState("");
+    const priorityRef = useRef();
+    const [status,setStatus] = useState("");
+    const statusRef = useRef();
+    const [customer, setCustomer] = useState("");
+    const customerRef = useRef();
 
     //get task collection from firestore database
-    const taskRef = collection(db, "tasks");
+    const taskCollectionRef = collection(db, "tasks");
     
-    function addTask(e){
-        taskObj.text = e.target.value
+    async function handleCreateTask(){
+
+        //add task object to firestore
+        try{
+            const docRef = await addDoc(taskCollectionRef,{
+                task: task,
+                date: date,
+                status: "",
+                priority: priority,
+                customer: ""
+            });
+            console.log("Document wrritten with ID: ", docRef.id);
+            
+            //clear inputs
+            taskRef.current.value = null;
+            dateRef.current.value = null;
+            //statusRef.current.value = null;
+            priorityRef.current.value = null;
+            //customerRef.current.value = null;
+            
+            //clear states
+            setTask("");
+            setDate("");
+            setPriority("");
+
+        }catch(e){
+            console.error("Error adding document: ", e);
+        } 
+        
     };
 
-    function addDate(e){
-        taskObj.date = e.target.value
-    };
-
-    function addStatus(e){
-        taskObj.status = e.target.value
-    };
-
-    let addCustomer = (e) => {taskObj.customer = e.target.value}
-    
-    return (
+    return(
         <div>
-            <h1>Create a task </h1>    
+            <h1>Create a task</h1>    
             <form>
-                <input placeholder='Task...' onChange={addTask} />
+                <input placeholder='Task...' ref={taskRef} onChange={e => {setTask(e.target.value);}}/> 
                 <br/>
-                <input type='Date' placeholder='Date...' onChange={addDate} />
-            </form> 
+                <input type='Date' ref={dateRef} onChange={e => {setDate(e.target.value)}}/>
+                <br/>
+                <select name="priority" ref={priorityRef} onChange={e => {setPriority(e.target.value)}}>
+                        <option value="Low Priorty">Low Priority</option>
+                        <option value="Medium Priority">Medium Priority</option>
+                        <option value="High Priority">High Priority</option>
+                </select>
+
+                <br/><br/>
+                <button type="button" onClick={handleCreateTask}>Submit</button>
+            </form>
         </div>
     );
 }
+
